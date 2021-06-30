c1b77cf3d0be0269f0f0e4df20e8f3d4cda1ef78
Leonardo <leonardoprado19@gmail.com>
2021-06-30 02:13:27 -0300

    Consertando problema de rota

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express(); // create express app
 const port = process.env.PORT || 3000;
 // add middlewares
+
 app.use(express.static(path.join(__dirname, "..", "build")));
+
 app.use(express.static("public"));
+
+app.get('*', (req, res) => {                       
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));                               
+});
 
 // start express server on port 5000
 app.listen(port, () => {
